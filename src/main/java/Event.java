bed56a8700985a6752deeb9fc88b28c4310b7d29
Timothy Herold <heroldt@gmail.com>
2016-09-08 21:10:24 -0700

    Add several event-cost related methods to Event class.

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,33 @@
 public class Event {
+  private Integer mAttendees;
+  private Meal mMeal;
 
   public Event() {
-
+    mAttendees = 0;
+    mMeal = new Meal();
   }
+
+  public Integer getNumberOfAttendees() {
+    return mAttendees;
+  }
+
+  public void setNumberOfAttendees(Integer attendees) {
+    mAttendees = attendees;
+  }
+
+  public void setMealId(Integer id) {
+    mMeal.setMealId(id);
+  }
+
+  public Double getTotalEventCost() {
+    Double baseCost = mAttendees * 5.00;
+    Double mealCost = mAttendees * getMealCostPerPerson();
+    return baseCost + mealCost;
+  }
+
+  public Double getMealCostPerPerson() {
+    return mMeal.getMealCost();
+  }
+
 
 }
--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -9,4 +9,32 @@ public class EventTest {
     assertEquals(true, testEvent instanceof Event);
   }
 
+  @Test
+  public void getNumberOfAttendees_returnsNumberOfAttendees_isInteger() {
+    Event testEvent = new Event();
+    assertEquals(true, testEvent.getNumberOfAttendees() instanceof Integer);
+  }
+
+  @Test
+  public void setNumberOfAttendees_returnsNumberOfAttendees_2() {
+    Event testEvent = new Event();
+    testEvent.setNumberOfAttendees(2);
+    Integer expected = 2;
+    assertEquals(expected, testEvent.getNumberOfAttendees());
+  }
+
+  @Test
+  public void getMealCostPerPerson_returnsMealCostPerPerson_000() {
+    Event testEvent = new Event();
+    testEvent.setMealId(0);
+    Double expected = 0.00;
+    assertEquals(expected, testEvent.getMealCostPerPerson());
+  }
+
+  @Test
+  public void getTotalEventCost_returnsCostOfEvent_isDouble() {
+    Event testEvent = new Event();
+    assertEquals(true, testEvent.getTotalEventCost() instanceof Double);
+  }
+
 }
