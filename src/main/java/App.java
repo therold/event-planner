5d058e3af9b4d6d8b35afd06b1a59c15f3d24533
Timothy Herold <heroldt@gmail.com>
2016-09-08 21:47:32 -0700

    Adjust front end to run continuously until the user asks to exit.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,27 +5,37 @@ public class App {
     Console console = System.console();
     Event event = new Event();
     Meal meals = new Meal();
+    boolean running = true;
+    while (running) {
+      System.out.println("Welcome to Epicodus Event Planners!");
+      System.out.println("We'll need to collect a bit of information about your upcoming event.");
 
-    System.out.println("Welcome to Epicodus Event Planners!");
-    System.out.println("We'll need to collect a bit of information about your upcoming event.");
+      System.out.println("How many people will be attending your upcoming event?");
+      System.out.print("People: ");
+      Integer people = Integer.parseInt(console.readLine());
+      event.setNumberOfAttendees(people);
+      System.out.println("Thanks!");
+      System.out.println();
 
-    System.out.println("How many people will be attending your upcoming event?");
-    System.out.print("People: ");
-    Integer people = Integer.parseInt(console.readLine());
-    event.setNumberOfAttendees(people);
-    System.out.println("Thanks!");
-    System.out.println();
+      System.out.println("What type of meal will we serve?");
+      System.out.println("The options are:");
+      for (String meal : meals.getAllMealNames()) {
+        System.out.println(meal);
+      }
+      String meal = console.readLine();
+      event.setMealByName(meal);
+      System.out.println("Thanks!");
+      System.out.println();
 
-    System.out.println("What type of meal will we serve?");
-    System.out.println("The options are:");
-    for (String meal : meals.getAllMealNames()) {
-      System.out.println(meal);
+      System.out.println(String.format("The total cost for your event is %f", event.getTotalEventCost()));
+
+      System.out.println("Would you like to plan another event?");
+      System.out.print("[Y/n] ");
+      String choice = console.readLine();
+      if (choice.equals("N") || choice.equals("n")) {
+        running = false;
+      }
     }
-    String meal = console.readLine();
-    event.setMealByName(meal);
-    System.out.println("Thanks!");
-    System.out.println();
 
-    System.out.println(String.format("The total cost for your event is %f", event.getTotalEventCost()));
   }
 }
